c83bb4a09c313e8bfa6b929b9b2905d7790c9304
amulyavm <amulya.mu@gmail.com>
2019-05-02 13:46:51 -0500

    Initial commit

---
+++ Calculate.java
@@ -0,0 +1,24 @@
+package firstProject;
+
+public class Calculate {
+
+	public static void main(String[] args) {
+		System.out.println("Hello everyone");
+		int x=5;
+		System.out.println("There are " + x+ " students");
+		message();
+		add();
+		// TODO Auto-generated method stub
+
+	}
+	public static void message() {
+	System . out . println("How are you guys today");
+
+}
+	public static void add() {
+		int a=10,b=15;
+				System.out.println("addition is " + (a+b));
+				
+}
+ 	
+}
